5aa46e61ab4693df30b11122fffca084662f3987
Gtp87 <fabio.giannitrapani@gmail.com>
2021-12-18 14:05:08 +0100

    bonus 1

---
+++ bonus/js/script.js
@@ -0,0 +1,13 @@
+// Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+// Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+// Poi aggiungiamo un pulsante per aumentare questo numero.
+
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        message: 'Hello World',
+        image: 'https://picsum.photos/300/300',
+        text: ''
+    },
+})
